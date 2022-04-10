daeef50f5eebe05c793b0a05b340951afa5f2f54
GemaMesasV <gema.mesas.v@gmail.com>
2022-04-10 19:55:43 +0200

    add to favourite list

--- src/js/1.global.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// CONSTANTS FOR SEARCH SECTION
-const inputSearch = document.querySelector(".js-inputSearch");
-const inputSearchBtn = document.querySelector(".js-btnSearch");
-const inputSearchForm = document.querySelector(".js-formSearch");
-const boxListFetch = document.querySelector(".js-boxListFetch");
-
-// CONSTANTS FOR ...
